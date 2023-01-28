d215fc57ac14a352993183ed144747ca96129b6e
ewilan-riviere <ewilan-riviere@users.noreply.github.com>
2023-01-28 14:04:53 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Kiwilan\TypeableModels\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Kiwilan\TypeableModels\TypeableModelsServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
