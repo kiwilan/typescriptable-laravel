a721c9cf6af3f42cbdd963ca3406e794416d4ac0
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-02-15 11:40:42 +0100

    InertiaEmbed

--- src/Services/Types/Inertia/InertiaEmbed.php
+++ src/Services/Types/Inertia/InertiaEmbed.php
@@ -4,30 +4,45 @@ namespace Kiwilan\Typescriptable\Services\Types\Inertia;
 
 class InertiaEmbed
 {
+    const PAGE = '$page: Inertia.Page';
+
+    const SESSIONS = 'sessions: { agent: { is_desktop: boolean; browser: string; platform: string; }, ip_address: string; is_current_device: boolean; last_active: string; }[];';
+
     public static function make(): string
     {
-        return <<<'typescript'
+        $route = '$route';
+        $isRoute = '$isRoute';
+        $currentRoute = '$currentRoute';
+        $ipage = '$ipage';
+        $page = self::PAGE;
+        $sessions = self::SESSIONS;
+
+        return <<<typescript
         declare module "@vue/runtime-core" {
           interface ComponentCustomProperties {
-            $route: (name: Route.Name, params?: Route.Params[Route.Name]) => string;
-            $isRoute: (name: Route.Name, params?: Route.Params[Route.Name]) => boolean;
-            $currentRoute: () => string;
-            $page: Inertia.Page
-            sessions: { agent: { is_desktop: boolean; browser: string; platform: string; }, ip_address: string; is_current_device: boolean; last_active: string; }[];
+		    {$route}: (name: Route.Name, params?: Route.Params[Route.Name]) => string;
+			{$isRoute}: (name: Route.Name, params?: Route.Params[Route.Name]) => boolean;
+			{$currentRoute}: () => string;
+			{$ipage}: Inertia.Page
+			{$page}
+			{$sessions}
           }
         }
-        typescript;
+typescript;
     }
 
     public static function native()
     {
-        return <<<'typescript'
-        declare module "@vue/runtime-core" {
-          interface ComponentCustomProperties {
-            $page: Inertia.Page
-            sessions: { agent: { is_desktop: boolean; browser: string; platform: string; }, ip_address: string; is_current_device: boolean; last_active: string; }[];
-          }
-        }
-        typescript;
+        $page = self::PAGE;
+        $sessions = self::SESSIONS;
+
+        return <<<typescript
+		declare module "@vue/runtime-core" {
+		  interface ComponentCustomProperties {
+			{$page}
+			{$sessions}
+		  }
+		}
+		typescript;
     }
 }
