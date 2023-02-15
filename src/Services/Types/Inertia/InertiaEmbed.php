d5b4566ac44d1c0bf5bea7a635dcb1aed462e25f
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-02-15 10:44:22 +0100

    clean

--- src/Services/Types/Inertia/InertiaEmbed.php
+++ src/Services/Types/Inertia/InertiaEmbed.php
@@ -9,8 +9,8 @@ class InertiaEmbed
         return <<<'typescript'
         declare module "@vue/runtime-core" {
           interface ComponentCustomProperties {
-            $route: (name: keyof ZiggyLaravelRoutes, params?: RouteParamsWithQueryOverload | RouteParam, absolute?: boolean, customZiggy?: Config) => string;
-            $isRoute: (name: keyof ZiggyLaravelRoutes, params?: RouteParamsWithQueryOverload) => boolean;
+            $route: (name: Route.Name, params?: Route.Params[Route.Name]) => string;
+            $isRoute: (name: Route.Name, params?: Route.Params[Route.Name]) => boolean;
             $currentRoute: () => string;
             $page: Inertia.Page
             sessions: { agent: { is_desktop: boolean; browser: string; platform: string; }, ip_address: string; is_current_device: boolean; last_active: string; }[];
