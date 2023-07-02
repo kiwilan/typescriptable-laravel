1e68837e1a8cb85f5c76999868a543dfa54dd567
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-07-02 10:46:11 +0200

    tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -9,4 +9,36 @@ define('DATABASE_TYPES', [
     'sqlsrv',
 ]);
 
+function outputDir(?string $file = null): string
+{
+    $currentDir = getcwd();
+
+    if ($file) {
+        return "{$currentDir}/tests/output/{$file}";
+    }
+
+    return "{$currentDir}/tests/output";
+}
+
+function models(): string
+{
+    $currentDir = getcwd();
+
+    return "{$currentDir}/tests/Data/Models";
+}
+
+function routes(): string
+{
+    $currentDir = getcwd();
+
+    return "{$currentDir}/tests/Data/routes.json";
+}
+
+function settings(): string
+{
+    $currentDir = getcwd();
+
+    return "{$currentDir}/tests/Data/Settings";
+}
+
 uses(TestCase::class)->in(__DIR__);
