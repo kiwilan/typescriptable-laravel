1913b25c4f28d05d7060727c93015c00ac3d5de9
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-01-13 14:19:28 +0100

    Refactor imports and export

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,47 +1,13 @@
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import type { DefineComponent } from 'vue'
-import {
-  useDate,
-  useFetch,
-  useInertia,
-  useLazy,
-  usePaginate,
-  usePagination,
-  useQuery,
-  useRouter,
-  useSearch,
-  useSidebar,
-  useSlideover,
-  useTypescriptable,
-} from './composables'
-import type {
-  Query,
-  SortItem,
-} from './composables'
 import { VueTypescriptable } from './vue-plugin'
 
 async function resolve(name: string, glob: Record<string, () => Promise<unknown>>): Promise<DefineComponent> {
   return resolvePageComponent(`./Pages/${name}.vue`, glob) as Promise<DefineComponent>
 }
 
-export type {
-  Query,
-  SortItem,
-}
-
+export * from './composables'
 export {
   VueTypescriptable,
   resolve,
-  useDate,
-  useFetch,
-  useInertia,
-  useLazy,
-  usePaginate,
-  usePagination,
-  useQuery,
-  useRouter,
-  useSearch,
-  useSidebar,
-  useSlideover,
-  useTypescriptable,
 }
