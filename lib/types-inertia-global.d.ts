383730a2eb108c74f6dac74fa913faeffe816e1d
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-02-02 17:34:32 +0100

    Update package.json and tsconfig.json

--- lib/types-inertia-global.d.ts
+++ lib/types-inertia-global.d.ts
@@ -8,7 +8,7 @@ declare module 'vue' {
     $to: <T extends App.Route.Name>(route: App.Route.RouteConfig<T>) => string
     // @ts-expect-error - Routes is defined in the global scope
     $page: Inertia.Page
-    sessions: { agent: { is_desktop: boolean; browser: string; platform: string }; ip_address: string; is_current_device: boolean; last_active: string }[]
+    sessions: { agent: { is_desktop: boolean, browser: string, platform: string }, ip_address: string, is_current_device: boolean, last_active: string }[]
   }
   export interface GlobalComponents {
     Head: typeof import('@inertiajs/vue3').Head
