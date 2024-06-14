bd80a83f86ce9de0196e8fc75d37a36f519104e0
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-06-14 08:20:55 +0200

    add medialibrary for tests

--- tests/Data/database/migrations/create_models_tables.php
+++ tests/Data/database/migrations/create_models_tables.php
@@ -188,6 +188,27 @@ return new class extends Migration
 
             $table->timestamps();
         });
+
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+
+            $table->morphs('model');
+            $table->uuid()->nullable()->unique();
+            $table->string('collection_name');
+            $table->string('name');
+            $table->string('file_name');
+            $table->string('mime_type')->nullable();
+            $table->string('disk');
+            $table->string('conversions_disk')->nullable();
+            $table->unsignedBigInteger('size');
+            $table->json('manipulations');
+            $table->json('custom_properties');
+            $table->json('generated_conversions');
+            $table->json('responsive_images');
+            $table->unsignedInteger('order_column')->nullable()->index();
+
+            $table->nullableTimestamps();
+        });
     }
 
     private function createTable(string $name): string
