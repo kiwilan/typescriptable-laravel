8db3193ad9b9982ddd06ad61617feb57949a9b58
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-07-22 18:18:12 +0200

    refactor: Add SchemaModelAttribute for key and hidden fields in ParserModelFillable
    This commit adds the SchemaModelAttribute for the key field and hidden fields in the ParserModelFillable class. The key field is set as unique and not fillable, while the hidden fields are set as nullable and fillable. This change improves the handling of key and hidden fields in the MongoDB schema model.

--- src/Typed/Eloquent/Parser/ParserModelFillable.php
+++ src/Typed/Eloquent/Parser/ParserModelFillable.php
@@ -28,11 +28,42 @@ class ParserModelFillable
         /** @var Model */
         $instance = new $model();
 
+        $key = $instance->getKeyName();
         $casts = $instance->getCasts();
+
+        if ($key) {
+            $self->attributes[$key] = SchemaModelAttribute::make('mongodb', new SchemaModelAttribute(
+                name: $key,
+                databaseType: 'string',
+                increments: false,
+                nullable: false,
+                default: null,
+                unique: true,
+                fillable: false,
+                hidden: false,
+                appended: false,
+                cast: $casts[$key] ?? null,
+            ));
+        }
+
+        foreach ($instance->getHidden() as $field) {
+            $self->attributes[$field] = SchemaModelAttribute::make('mongodb', new SchemaModelAttribute(
+                name: $field,
+                databaseType: 'string',
+                increments: false,
+                nullable: true,
+                default: null,
+                unique: false,
+                fillable: true,
+                hidden: true,
+                appended: false,
+                cast: $casts[$field] ?? null,
+            ));
+        }
 
         foreach ($instance->getFillable() as $field) {
             $self->attributes[$field] = SchemaModelAttribute::make('mongodb', new SchemaModelAttribute(
-                name: 'title',
+                name: $field,
                 databaseType: 'string',
                 increments: false,
                 nullable: true,
